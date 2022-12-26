024a038870f8f4df68b5fb05188aa8b39ecf9d1c
yhuj79 <yu1085@naver.com>
2022-12-26 18:28:35 +0900

    디렉터리 정리

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### COVID-STAT built with React
 
-<img src=https://raw.githubusercontent.com/yhuj79/COVID-STAT/master/thumnail/1.PNG width=600>
-<img src=https://raw.githubusercontent.com/yhuj79/COVID-STAT/master/thumnail/2.PNG width=600>
+<img src=https://raw.githubusercontent.com/yhuj79/COVID-STAT/master/thumbnail/1.PNG width=600>
+<img src=https://raw.githubusercontent.com/yhuj79/COVID-STAT/master/thumbnail/2.PNG width=600>
--- covidstatkr/src/App.js
+++ covidstatkr/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import moment from 'moment';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
 
-import Loader from './Loader';
-import Header from './Header';
-import DecideDaily from './DecideDaily';
-import DeathDaily from './DeathDaily';
-import DecideCumulative from './DecideCumulative';
-import DeathCumulative from './DeathCumulative';
-import TextMobile from './TextMobile';
-import Source from './Source';
+import Loader from "./Loader";
+import Header from "./Header";
+import DecideDaily from "./DecideDaily";
+import DeathDaily from "./DeathDaily";
+import DecideCumulative from "./DecideCumulative";
+import DeathCumulative from "./DeathCumulative";
+import TextMobile from "./TextMobile";
+import Source from "./Source";
 
 // date
 const format = "YYYYMMDD";
@@ -21,26 +21,26 @@ const termB = moment().subtract(15, "days").format(format);
 function App() {
   const [stat, setStat] = useState(null);
   const [loading, setLoading] = useState(false);
-  const serviceKey = "PDQhKwjgEGznnLng2oat2Q5kYgA%2BIj6fx%2BN1FXppnbqJ9wR2x26%2BQ7gnH7RLePFK27SPRF5F1pllsDQj4nXa%2Fg%3D%3D";
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios?.get(
-          `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=${serviceKey}&pageNo=1&numOfRows=10&startCreateDt=${termB}&endCreateDt=${termA}`,
-        )
-        setStat(response.data.response.body.items.item);
+        const res = await axios?.get(
+          `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=${process.env.REACT_APP_API_KEY}&pageNo=1&numOfRows=10&startCreateDt=${termB}&endCreateDt=${termA}`
+        );
+        setStat(res.data.response.body.items.item);
+        console.log(res.data.response.body.items.item);
       } catch (e) {
         console.log(e);
       }
       setLoading(false);
-    }
+    };
     fetchData();
-  }, [])
+  }, []);
 
   if (loading) {
-    return <Loader />
+    return <Loader />;
   }
   if (!stat) {
     return null;
@@ -53,8 +53,14 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DecideDaily stat={stat} />} />
           <Route path="/deathdaily" element={<DeathDaily stat={stat} />} />
-          <Route path="/decidecumulative" element={<DecideCumulative stat={stat} />} />
-          <Route path="/deathcumulative" element={<DeathCumulative stat={stat} />} />
+          <Route
+            path="/decidecumulative"
+            element={<DecideCumulative stat={stat} />}
+          />
+          <Route
+            path="/deathcumulative"
+            element={<DeathCumulative stat={stat} />}
+          />
         </Routes>
         <TextMobile />
         <Source />
